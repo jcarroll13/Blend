70d1c553d321de4af25fce562487892ce5cfcbe0
msalmon14 <msalmon14@ITIC-042-9020M.eil-server.cba.ua.edu>
2015-09-24 17:38:25 -0500

    Hi

--- Purple/src/Purple.java
+++ Purple/src/Purple.java
@@ -3,7 +3,7 @@ public class Purple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Michael");
+		System.out.println("Michael Salmon");
 	}
 
 }
