ed50a93af31cb1f8c7c58c8c7b6583b7c3c57cdf
msalmon14 <msalmon14@ITIC-042-9020M.eil-server.cba.ua.edu>
2015-09-28 19:10:26 -0500

    # WARNING: head commit changed in the meantime
    Hi

--- Purple/src/Purple.java
+++ Purple/src/Purple.java
@@ -4,9 +4,7 @@ public class Purple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Michael Salmon");
-		
-		If(var x = 0){
-			var y = 2
+
 		}
 	}
 
